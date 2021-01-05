b2ab8c7dae0055d2f47db969e88845b1855eb0a6
Constantine Mamakos <mamakos@andrew.cmu.edu>
2021-01-05 16:37:52 -0500

    Initial Commit

--- src/main/java/com/mamakos/StringCalculator.java
+++ src/main/java/com/mamakos/StringCalculator.java
@@ -8,15 +8,19 @@ public class StringCalculator {
     public static int add(String numbers) throws Exception {
         if (numbers.equals("")) return 0;
 
+        //figure out whether a custom delimiter is used
         String delimiter = ",";
         if (customDelimiterExists(numbers)) {
-            delimiter = numbers.substring(0, 1); //default delimiter
+            delimiter = numbers.substring(0, 1);
             numbers = numbers.substring(3);
         }
 
+        //prepare error handling
         boolean errorFlag = false;
         StringBuilder errorMsg = new StringBuilder();
         errorMsg.append("Error: Negatives not allowed ");
+
+        //process list
         int total = 0;
         numbers = numbers.replace(System.lineSeparator(), delimiter);
         String[] strArray = numbers.split(delimiter);
@@ -30,6 +34,8 @@ public class StringCalculator {
                 total += myInteger;
             }
         }
+
+        //return results or throw error
         if(errorFlag){
             throw new Exception(errorMsg.toString());
         }
